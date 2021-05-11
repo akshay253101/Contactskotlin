19dbbecaa5fde8821b1af944bdf96711421b30a6
Akshay Yadav <akshay253101yadav@gmail.com>
2021-05-11 19:08:04 +0530

    Base Url Updated

--- app/src/main/java/com/example/contacts/kotlin/utils/Constants.kt
+++ app/src/main/java/com/example/contacts/kotlin/utils/Constants.kt
@@ -3,6 +3,6 @@ package com.example.contacts.kotlin.utils
 import java.util.ArrayList
 
 /** The base URL of the API */
-const val BASE_URL: String = "https://raw.githubusercontent.com/akshay253101/ContactKotlin/master/"
+const val BASE_URL: String = "https://raw.githubusercontent.com/akshay253101/Contactskotlin/master/"
 var listArray: ArrayList<String> = ArrayList()
 var needSeparator : Boolean = true
